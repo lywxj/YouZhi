dd1177a27e90c1e00e4500582fd60a648bd69f9f
arvin <rorogestudio@gmail.com>
2015-03-03 16:45:46 +0800

    add WoDiVC

---
+++ YouZhi/Project/WoDi/WoDiVC.h
@@ -0,0 +1,13 @@
+//
+//  WoDiVC.h
+//  YouZhi
+//
+//  Created by Arvin on 15/3/3.
+//  Copyright (c) 2015年 com.roroge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WoDiVC : UIViewController
+
+@end
---
+++ YouZhi/Project/WoDi/WoDiVC.m
@@ -0,0 +1,27 @@
+//
+//  WoDiVC.m
+//  YouZhi
+//
+//  Created by Arvin on 15/3/3.
+//  Copyright (c) 2015年 com.roroge. All rights reserved.
+//
+
+#import "WoDiVC.h"
+
+@interface WoDiVC ()
+
+@end
+
+@implementation WoDiVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
